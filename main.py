f92894fa2b90d651620eeac36704d757aaed983d
Gradimwamba <gradimwamba25@gmail.com>
2026-01-30 23:29:11 +0100

    Début de mon programme

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Voici notre premier project
 # nous voulons un code python qui permet a l'utilisateur de deviner un nombre:
 # Entrer un nombre, et quand l'utilisateur entre le nombre, ça dit si le nombre entré est grand ou petit. ou il a trouver le bon nombre
+
+
+number = int(input("Entrer un nombre : ? "))
+
+if number >= 100 :
+    print("Le nombre entré est grand")
+else :
+    print("Le nombre entré est petit")
